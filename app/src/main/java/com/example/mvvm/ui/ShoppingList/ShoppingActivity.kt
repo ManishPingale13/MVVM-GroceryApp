c3869a0771593378e0e324cffe17997705f59e8c
Manish Pingale <manishpingale2005@gmail.com>
2020-11-26 17:03:02 +0530

    Added Dialog,Room DB is now functional (fixed warnings)

--- app/src/main/java/com/example/mvvm/ui/ShoppingList/ShoppingActivity.kt
+++ app/src/main/java/com/example/mvvm/ui/ShoppingList/ShoppingActivity.kt
@@ -2,10 +2,15 @@ package com.example.mvvm.ui.ShoppingList
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvm.R
 import com.example.mvvm.data.Repositories.ShoppingRepo
+import com.example.mvvm.data.db.Entities.ShoppingItem
 import com.example.mvvm.data.db.ShoppingDB
+import com.example.mvvm.other.ShoppingItemAdapter
+import kotlinx.android.synthetic.main.activity_shopping.*
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +22,22 @@ class ShoppingActivity : AppCompatActivity() {
         val factory = ShoppingVMFactory(repo)
 
         val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
+
+        val adapter = ShoppingItemAdapter(listOf(), viewModel)
+        shopping_rv.layoutManager = LinearLayoutManager(this)
+        shopping_rv.adapter = adapter
+
+        viewModel.getAllShoppingItems().observe(this, Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
+
+        shoppingFab.setOnClickListener {
+            AddShoppingItemDialog(this, object : AddDialogListener {
+                override fun onAddButtonClicked(item: ShoppingItem) {
+                    viewModel.upsert(item)
+                }
+            }).show()
+        }
     }
 }
